bb544c70fbacd618383310a7c379835ff515c438
RamosDominguez23 <sabresresio@gmail.com>
2026-03-29 12:43:24 -0500

    Test 2: validación de nombre

--- src/test/java/PedidoTest.java
+++ src/test/java/PedidoTest.java
@@ -7,6 +7,12 @@ public class PedidoTest {
         Pedido pedido = new Pedido("P123", "Carlos", 10, 50.5, java.time.LocalDate.now());
         assertEquals("El pedido ha sido registrado correctamente", pedido.validar());
     }
+
+    @Test
+    void nombreClienteInvalido() {
+        Pedido pedido = new Pedido("P123", "Jo", 10, 50.5, java.time.LocalDate.now());
+        assertEquals("El nombre del cliente debe tener al menos tres caracteres alfabéticos", pedido.validar());
+    }
 }
 
 
